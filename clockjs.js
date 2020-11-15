3de76295b156d4cc75069dc74430d2ae1c39067a
Sadia Islam <37986610+SMShoron@users.noreply.github.com>
2020-11-16 00:02:51 +0600

    added js file in html

--- clockjs.js
+++ clockjs.js
@@ -12,7 +12,18 @@ function setClock() {
     const secondsRatio = currentDate.getSeconds()/60   //second hand ta koto tuk rotate hbe sheta declare kora hoise. const keyword ta define kore j variable ta constant.
     const minutesRatio = (secondsRatio + currentDate.getMinutes())/60
     const hoursRatio = (minutesRatio + currentDate.getHours())/12
+  
+    //setRotation function ta call kora hoise.
+    setRotation(secondHand , secondsRatio)
+    setRotation(minuteHand , minutesRatio)
+    setRotation(hourHand , hoursRatio)
 }
 
 
 //Set the value for rotation of the hands
+function setRotation(element,rotatRatio){
+    element.style.setProperty('--rotation',rotatRatio *360) // property method e css e declare kora rotation method ta set kora hoyeche.
+
+}
+
+setClock() //page load newar sathe sathe exact time theke clock start hbe.
